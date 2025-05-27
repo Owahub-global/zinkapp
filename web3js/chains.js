749aabcd029192d0972e6c846134684799c33bbb
Owahub-global <owahubavenue@gmail.com>
2025-05-27 23:28:53 +0100

    Add files via upload

--- web3js/chains.js
+++ web3js/chains.js
@@ -1,15 +1,15 @@
 
 const chains = {
-  97: {
+  56: {
     name: "BNB Testnet",
     rpc: "https://data-seed-prebsc-1-s1.binance.org:8545/",
-    presaleAddress: "0x3B4f322A9e7d9a3fa035825BBA13177C60b7F1E0",
-    tokenAddress: "0x299f4361Be1cC70A6DbF3daDF711Fbc5272CD59D",
+    presaleAddress: "0x0B633B2D4eb5B41b1BFf20c842AB8C1e3630FE56",
+    tokenAddress: "0xb09f8C7B524C9bCaa59582DC01982213D6a7c046",
     presaleAbi,
     tokenAbi
   }
 };
 
 const rpcMap = {
-  97: chains[97].rpc
+  56: chains[56].rpc
 };
